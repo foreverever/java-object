d9c052e0097dd980db45db59c10a6ebe30e047ed
TaeSu <lollolzkk@gmail.com>
2019-08-11 22:55:16 +0900

    [chapter01] 티켓 판매 시스템_step02 complete (객체간 결합도 낮추기_Theater 클래스 내 enter() 메서드 수정(ticketSeller 객체에게 책임 위임)

--- src/main/java/chapter01/seller/TicketSeller.java
+++ src/main/java/chapter01/seller/TicketSeller.java
@@ -1,5 +1,8 @@
 package chapter01.seller;
 
+import chapter01.audience.Audience;
+import chapter01.ticket.Ticket;
+
 public class TicketSeller {
     private TicketOffice ticketOffice;
 
@@ -9,5 +12,19 @@ public class TicketSeller {
 
     public TicketOffice getTicketOffice() {
         return ticketOffice;
+    }
+
+    public void sellTo(Audience audience) {
+        if (audience.getBag().hasInvitation()) {
+            Ticket ticket = ticketOffice.getTicket();
+            audience.getBag().setTicket(ticket);
+        } else {
+            Ticket ticket = ticketOffice.getTicket();
+            audience.getBag().minusAmount(ticket.getFee());
+            ticketOffice.plusAmount(ticket.getFee());
+            audience.getBag().setTicket(ticket);
+        }
+        System.out.println("티켓 판매 완료!");
+        System.out.println("남은 티켓 개수 : " + ticketOffice.remainderTicket());
     }
 }
--- src/main/java/chapter01/theater/Theater.java
+++ src/main/java/chapter01/theater/Theater.java
@@ -2,7 +2,6 @@ package chapter01.theater;
 
 import chapter01.audience.Audience;
 import chapter01.seller.TicketSeller;
-import chapter01.ticket.Ticket;
 
 public class Theater {
     private TicketSeller ticketSeller;
@@ -11,18 +10,8 @@ public class Theater {
         this.ticketSeller = ticketSeller;
     }
 
-    //절차 지향적
+    //1차 개선(ticketSeller에게 책임 위임)
     public void enter(Audience audience) {
-        if (audience.getBag().hasInvitation()) {
-            Ticket ticket = ticketSeller.getTicketOffice().getTicket();
-            audience.getBag().setTicket(ticket);
-        } else {
-            Ticket ticket = ticketSeller.getTicketOffice().getTicket();
-            audience.getBag().minusAmount(ticket.getFee());
-            ticketSeller.getTicketOffice().plusAmount(ticket.getFee());
-            audience.getBag().setTicket(ticket);
-        }
-        System.out.println("티켓 판매 완료!");
-        System.out.println("남은 티켓 개수 : " + ticketSeller.getTicketOffice().remainderTicket());
+        ticketSeller.sellTo(audience);
     }
 }
